d7aaa6813bbca2e51aef24a6705d9970a73c3a7c
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-02-17 20:10:38 -0600

    Create sort_two_numbers_descending.cu

---
+++ easy/sort_two_numbers_descending.cu
@@ -0,0 +1,30 @@
+// https://cudaforces.com/problem/11
+
+__global__ void kernel(int* cudaData) {
+    if (threadIdx.x == 0) {
+        if (cudaData[0] < cudaData[1]) {
+            int tmp = cudaData[0];
+            cudaData[0] = cudaData[1];
+            cudaData[1] = tmp;
+        }
+    }
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int hostData[2] = {a, b};
+    
+    int* cudaData;
+    cudaMalloc(cudaData, 2 * sizeof(int));
+
+    cudaMemcpy(cudaData, hostData, 2 * sizeof(int), cudaMemcpyHostToDevice);
+    kernel<<<1, 1>>>(cudaData);
+    cudaMemcpy(hostData, cudaData, 2 * sizeof(int), cudaMemcpyDeviceToHost);
+    
+    printf("%d %d", hostData[0], hostData[1]);
+
+    cudaDeviceSynchronize();
+    cudaFree(cudaData);
+}
