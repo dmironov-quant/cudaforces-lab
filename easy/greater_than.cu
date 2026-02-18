f82881b1e9e7addf66c249e5fe2a094630c50f67
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-02-17 20:13:10 -0600

    Create greater_than.cu

---
+++ easy/greater_than.cu
@@ -0,0 +1,32 @@
+// https://cudaforces.com/problem/33
+
+__global__ void kernel(int* first, int* second, int* ans) {
+    if (threadIdx.x == 0) {
+        *ans = (*first > *second) ? 1 : 0;
+    }
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int *first, *second, *ans;
+    cudaMalloc(&first, sizeof(int));
+    cudaMalloc(&second, sizeof(int));
+    cudaMalloc(&ans, sizeof(int));
+
+    cudaMemcpy(first, &a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(second, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+    kernel<<<1, 1>>>(first, second, ans);
+    cudaDeviceSynchronize();
+
+    int res;
+    cudaMemcpy(&res, ans, sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("%d", res);
+
+    cudaFree(first);
+    cudaFree(second);
+    cudaFree(ans);
+}
