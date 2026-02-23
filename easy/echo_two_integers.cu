dc70b0d4847d21f0da18b56dcb9906287c8511a5
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-02-23 12:24:32 -0600

    Create echo_two_integers.cu

---
+++ easy/echo_two_integers.cu
@@ -0,0 +1,26 @@
+// https://cudaforces.com/problem/20
+
+__global__ void kernel(int *d_a, int *d_b) {
+    if (threadIdx.x == 0) {
+        printf("%d %d", *d_a, *d_b);
+    }
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int *d_a = nullptr;
+    int *d_b = nullptr;
+    cudaMalloc(&d_a, sizeof(int));
+    cudaMalloc(&d_b, sizeof(int));
+
+    cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+    kernel<<<1, 1>>>(d_a, d_b);
+    cudaDeviceSynchronize();
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+}
